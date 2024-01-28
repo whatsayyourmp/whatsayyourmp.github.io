6c85a8b6d0c9caca3d229e25b7288a3dcfcb83b7
Jing Wei <jingwei.chew@gmail.com>
2024-01-28 13:31:34 -0500

    ci: setup Sentry

--- src/index.js
+++ src/index.js
@@ -1,8 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as Sentry from "@sentry/react";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import constants from './constants'
+
+const SENTRY_DSN = "https://84525b373b9474b69bd9a6243c119d22@o4506650230194176.ingest.sentry.io/4506650231898112";
+
+Sentry.init({
+  dsn: SENTRY_DSN,
+  integrations: (constants.STAGE === 'production') && [
+    new Sentry.BrowserTracing({      
+      tracePropagationTargets: ['https://hidden-pad-313204.as.r.appspot.com'],
+    }),
+    new Sentry.Replay({
+      maskAllText: false,
+      blockAllMedia: false,
+    }),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
 
 ReactDOM.render(
   <React.StrictMode>
