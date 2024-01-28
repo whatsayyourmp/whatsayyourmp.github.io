dcae763d3871f5241e781c84a0affa1b1916c787
Jing Wei <jingwei.chew@gmail.com>
2024-01-28 10:20:39 -0500

    ci: add local development configs

--- src/constants.js
+++ src/constants.js
@@ -2,17 +2,19 @@ import dayjs from 'dayjs';
 
 const PARLIAMENT_START_DATE = '2020-08-24';
 
-const stage = process.env.NODE_ENV;
+const STAGE = process.env.NODE_ENV;
 
 const localHostName = process.env.HOST ?? 'localhost'
 
 const GET_COUNTS_PER_MP_FOR_DATE_RANGE_URL_PER_STAGE = {
-  default: `http://${localHostName}:6969/counts-per-mp`,
+  local: `http://${localHostName}:6969/counts-per-mp`,
+  default: 'https://hidden-pad-313204.as.r.appspot.com/counts-per-mp',
   production: 'https://hidden-pad-313204.as.r.appspot.com/counts-per-mp',
 };
 
 const GET_CURRENT_MPS_URL_PER_STAGE = {
-  default: `http://${localHostName}:6969/current-mps`,
+  local: `http://${localHostName}:6969/current-mps`,
+  default: 'https://hidden-pad-313204.as.r.appspot.com/current-mps',
   production: 'https://hidden-pad-313204.as.r.appspot.com/current-mps',
 };
 
@@ -21,11 +23,13 @@ const Constants = {
   DEFAULT_COUNT_RANGE_START_DATE: dayjs(new Date(PARLIAMENT_START_DATE)),
 
   GET_COUNTS_PER_MP_FOR_DATE_RANGE:
-    GET_COUNTS_PER_MP_FOR_DATE_RANGE_URL_PER_STAGE[stage] ??
+    GET_COUNTS_PER_MP_FOR_DATE_RANGE_URL_PER_STAGE[STAGE] ??
     GET_COUNTS_PER_MP_FOR_DATE_RANGE_URL_PER_STAGE.default,
   GET_CURRENT_MPS:
-    GET_CURRENT_MPS_URL_PER_STAGE[stage] ??
+    GET_CURRENT_MPS_URL_PER_STAGE[STAGE] ??
     GET_CURRENT_MPS_URL_PER_STAGE.default,
+
+  STAGE
 };
 
 export default Constants;
--- src/services/index.js
+++ src/services/index.js
@@ -1,12 +1,23 @@
 import axios from 'axios';
 import constants from '../constants';
 
+const api = axios.create({
+  baseURL: process.env.REACT_APP_API_URL,
+  headers: {
+    ...{
+      'Content-Type': 'application/json'
+    }, ...(constants.STAGE !== 'production') ? {
+      'Authorization': localStorage.getItem('DEVELOPER_API_KEY'),
+    } : {}
+  }
+});
+
 const getCurrentMPs = () => {
-  return axios.get(constants.GET_CURRENT_MPS);
+  return api.get(constants.GET_CURRENT_MPS);
 };
 
 const getCountsPerMPForDateRange = async (mpNames, startDate, endDate) => {
-  return axios.get(constants.GET_COUNTS_PER_MP_FOR_DATE_RANGE, {
+  return api.get(constants.GET_COUNTS_PER_MP_FOR_DATE_RANGE, {
     params: {
       mpNames,
       startDate: startDate.$d,
